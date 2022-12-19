7909a6032c46abc378ba2851a0084d965985aad9
AgnieszkaCieslak <114153531+AgnieszkaCieslak@users.noreply.github.com>
2022-12-19 18:19:51 +0000

    Deploying to gh-pages from @ AgnieszkaCieslak/Team-A-Project@afb90ca1a1856a450921203b69a87887c46bf1cc 🚀

--- index.c54953d8.js
+++ index.c54953d8.js
@@ -1,2 +1,2 @@
 const e=document.querySelector(".mobile-menu"),t=document.querySelector(".mobile-menu__button");document.querySelector(".header__button").addEventListener("click",(t=>{e.classList.remove("d-none")})),t.addEventListener("click",(t=>{e.classList.add("d-none")}));
-//# sourceMappingURL=index.bc53c704.js.map
+//# sourceMappingURL=index.c54953d8.js.map
