f0b4d44a938e8fd25d41b4728e7c12b0c5f59a03
AgnieszkaCieslak <114153531+AgnieszkaCieslak@users.noreply.github.com>
2022-12-21 19:37:09 +0000

    Deploying to gh-pages from @ AgnieszkaCieslak/Team-A-Project@53deb132510048d12f5d36e4e97f33a7cae0afd9 🚀

---
+++ index.21418de1.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0})}));
+//# sourceMappingURL=index.21418de1.js.map
--- index.cec09aa2.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".slider").slick({dots:!0})}));
-//# sourceMappingURL=index.cec09aa2.js.map
